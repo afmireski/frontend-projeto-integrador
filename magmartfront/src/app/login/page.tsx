cc143c12349415524b5fe98716fab8e7778bef07
Victor Angelo <51969107+Victor476@users.noreply.github.com>
2024-08-01 14:53:53 -0300

    subirndo alteração

--- magmartfront/src/app/login/page.tsx
+++ magmartfront/src/app/login/page.tsx
@@ -1,8 +1,10 @@
 // src/app/login/page.tsx
 
-"use client"; // Adicione esta linha no início do arquivo
+"use client";
 
 import { useState } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie'; // Biblioteca para manipular cookies no frontend
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
@@ -12,20 +14,32 @@ export default function LoginPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:3001/sign-in', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-      credentials: 'include', // Inclui cookies na solicitação
-    });
+    try {
+      const response = await axios.post('http://localhost:3001/sign-in', {
+        email,
+        password,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
 
-    if (response.ok) {
-      window.location.href = '/'; // Redireciona após login bem-sucedido
-    } else {
-      const result = await response.json();
-      setError(result.message);
+      if (response.status === 200) {
+        const token = response.data.token;
+
+        // Armazene o token em um cookie
+        Cookies.set('authToken', token, { 
+          expires: 7, // O token expira em 7 dias
+          secure: true, // O cookie só será enviado em conexões HTTPS
+          sameSite: 'strict' // Protege contra CSRF
+        });
+
+        window.location.href = '/'; // Redireciona após login bem-sucedido
+      } else {
+        setError('Login failed');
+      }
+    } catch (error) {
+      setError('An error occurred during login');
     }
   };
 
