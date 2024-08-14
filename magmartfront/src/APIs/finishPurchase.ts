41e943a3af8981cc5b9bacc80d06942ef84c0d41
Victor Angelo <51969107+Victor476@users.noreply.github.com>
2024-08-14 19:31:24 -0300

    subindo alterações no purchase

--- magmartfront/src/APIs/finishPurchase.ts
+++ magmartfront/src/APIs/finishPurchase.ts
@@ -1,22 +1,36 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 async function FinishPurchase() {
-    return Promise.resolve(axios.post(`${process.env.API_URL}/purchases/finish`,{
-        "user_id": "e0353a92-d5b2-4ae7-af00-b9947eb72ea6",
-        "cart_id": "10c9f5f8-26de-4b3e-a977-685f416503e1",
-        "payment_method_id": "8c75baa8-ac41-4bff-a8a0-89c777ecde27"
-    }))
-    .then(response => {
-        if(response.status.toString() == '200'){
+    const token = Cookies.get('authToken');
+
+    try {
+        const response = await axios.post(
+            `${process.env.API_URL}/purchases/finish`, // URL da API
+            { // Dados do corpo da requisição
+                "user_id": "e0353a92-d5b2-4ae7-af00-b9947eb72ea6",
+                "cart_id": "10c9f5f8-26de-4b3e-a977-685f416503e1",
+                "payment_method_id": "8c75baa8-ac41-4bff-a8a0-89c777ecde27"
+            },
+            { // Cabeçalhos
+                headers: {
+                    'Authorization': `${token}`, // Adiciona o token ao cabeçalho
+                    'Content-Type': 'application/json',
+                }
+            }
+        );
+
+        if (response.status === 200) {
             console.log('Compra realizada com sucesso:', response.data);
             return response.data;
-        } 
-        return response.data;
-    })
-    .catch(error => {
+        } else {
+            // Retorna dados mesmo em caso de status diferente de 200
+            return response.data;
+        }
+    } catch (error) {
         console.error('Erro ao realizar a compra:', error);
-        throw('error');
-    });
+        throw error; // Lança o erro original para tratamento adequado
+    }
 }
 
 export default FinishPurchase;
