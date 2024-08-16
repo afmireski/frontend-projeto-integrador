e6c2ab3d9735604772c376d35d096a608e7ed5ed
Alexandre Tolomeotti <alexandretolomeotti@alunos.utfpr.edu.br>
2024-08-15 22:07:31 -0300

    WIP on #34
    The following code is going to be used on presentation for release 2 and contains many workarounds. Best practices will be added for release 3.

--- magmartfront/src/app/pokemon/[id]/page.tsx
+++ magmartfront/src/app/pokemon/[id]/page.tsx
@@ -13,33 +13,105 @@ import GetPokemon from '@/APIs/getPokemon';
 import addToCart from '@/app/api/addToCart';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import slugify from '@/utils/string';
+
+
+const getNamingColorByType = (type: string) => {
+    const gradientColors = {
+        from: "",
+        to: "",
+    };
+
+    switch(type) {
+        case "bug":
+            gradientColors.from = "from-green-400";
+            gradientColors.to = "to-yellow-300";
+            break;
+        case "poison":
+            gradientColors.from = "from-purple-600";
+            gradientColors.to = "to-purple-300";
+            break;
+        case "water":
+            gradientColors.from = "from-blue-500";
+            gradientColors.to = "to-blue-300";
+            break;
+        case "flying":
+            gradientColors.from = "from-blue-300";
+            gradientColors.to = "to-purple-200";
+            break;
+        case "normal":
+            gradientColors.from = "from-gray-400";
+            gradientColors.to = "to-gray-200";
+            break;
+        case "fire":
+            gradientColors.from = "from-orange-500";
+            gradientColors.to = "to-yellow-400";
+            break;
+        case "grass":
+            gradientColors.from = "from-green-500";
+            gradientColors.to = "to-green-300";
+            break;
+    }
+
+    return gradientColors;
+}
 
 const getTypeColor = (type: string) => {
     switch(type) {
         case "bug":
-            return "#a6b61f";
+            return "green-400";
         case "poison":
-            return "#904391";
+            return "purple-600";
         case "water":
-            return "#3091f2";
+            return "blue-500";
         case "flying":
-            return "#92a3f1";
+            return "blue-300";
         case "normal":
-            return "#c3bcb2";
+            return "gray-400";
         case "fire":
-            return "#e73b0d";
+            return "orange-500";
         case "grass":
-            return "#6fc033";
+            return "green-500";
+    }
+}
+
+const getHoverColor = (type: string) => {
+    switch(type) {
+        case "bug":
+            return "hover:bg-green-500";
+        case "poison":
+            return "hover:bg-purple-700";
+        case "water":
+            return "hover:bg-blue-600";
+        case "flying":
+            return "hover:bg-blue-400";
+        case "normal":
+            return "hover:bg-gray-500";
+        case "fire":
+            return "hover:bg-orange-600";
+        case "grass":
+            return "hover:bg-green-600";
     }
 }
 
 const PokemonTypeButton = styled(Button)({
-    borderRadius: '20px',
+    borderRadius: '15px',
     border: '2px solid black',
     color: 'white',
     width: '10rem',
     fontSize: '15px',
     boxShadow: '1px 1px 40px 0px rgba(255, 255, 255, 0.1) inset'
+})
+
+const PokemonTierButton = styled(Button)({
+    borderRadius: '0px',
+    border: '2px solid black',
+    color: 'white !important',
+    width: '7rem',
+    fontSize: '13px',
+    boxShadow: '1px 1px 40px 0px rgba(255, 255, 255, 0.1) inset',
+    fontWeight: '700',
 })
 
 function Product({params}: {params: {id: string}}) {
@@ -98,6 +170,33 @@ function Product({params}: {params: {id: string}}) {
         addToCart(userId, pokemonId, qtty);
     };
 
+    const { from, to } = getNamingColorByType(type.split(' ')[0]);
+
+    const getCardColor = () => {
+        const tmp = type.split(' ');
+        console.log(tmp);
+        if (tmp.length > 1) {
+            const primaryColor = getTypeColor(tmp[0])?.replace(/\d{3}$/, '500');
+            const secondaryColor = getTypeColor(tmp[1])?.replace(/\d{3}$/, '500');
+            // return `from-${primaryColor} to-${secondaryColor}`;
+            return `to-${getTypeColor(tmp[1])} from-${getTypeColor(tmp[0])}`;
+        }
+        return `bg-${getTypeColor(tmp[0])}`;
+    }
+
+    const getTierBGColor = () => {
+        const tier = slugify(tier_name);
+
+        switch(tier) {
+            case "inicial": return "bg-gray-300";
+            case "comum": return "bg-gray-500";
+            case "incomum": return "bg-blue-400";
+            case "raro": return "bg-orange-400";
+            case "epico": return "bg-purple-700";
+            case "lendario": return "bg-yellow-500";
+        }
+    }
+
   return (
       <div>
           <Navbar />
@@ -106,21 +205,34 @@ function Product({params}: {params: {id: string}}) {
                 <img src={image} alt={name}></img>
             </div>
             <div className='description tracking-wide'>
-                {/* <h1 className="font-normal uppercase" style={{ fontFamily: '"Flexo-Regular",arial,sans-serif', fontSize: '1.5rem' }}>{name}</h1><br></br> */}
-                <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-green-400">{name}</span></h1>
-                {type.split(' ').map((t) => (
-                        <PokemonTypeButton style={{ backgroundColor: getTypeColor(t) }} className="mr-4 font-mono font-black align-middle">{t}</PokemonTypeButton>
+                <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+                    <span className={`text-transparent bg-clip-text bg-gradient-to-r ${from} ${to}`}>
+                        {name} Nº {(ref_id).toString().padStart(4, '0')}
+                    </span>
+                </h1>
+                {type.split(' ').map((t, key) => (
+                    <PokemonTypeButton key={key} className={`bg-${getTypeColor(t)} ${getHoverColor(t)} mr-4 font-mono font-black align-middle`}>{t}</PokemonTypeButton>
                 ))}
-                <p className={`m-0 max-w-[100ch] text-sm text-balance`}>
-                    Exp: {exp} <br/>
-                    Weight: {weight}<br/>
-                    Height: {height}<br/>
-                    Tier: {tier_name}<br/>
-                </p>
-                <p className={`m-0 max-w-[100ch] text-sm text-balance`}>
-                    Price: {price} <br/>
-                    In Stock: {stock}
-                </p>
+                <Grid style={{ fontSize: '17px', width: '25rem' }} className={`bg-${getTypeColor( type.split(' ')[0] )} rounded-md font-mono font-semibold text-white my-5 mx-2`} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                {/* <Grid className={`bg-gradient-to-br ${getCardColor()} rounded-md font-mono font-semibold text-white my-5 mx-2`} style={{ fontSize: '17px', width: '25rem' }} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}> */}
+                    <Grid item xs={6} className="mt-3 mb-4">
+                        <p>Altura</p>
+                        <p className='text-black'>{height} m</p>
+                    </Grid>
+                    <Grid item xs={6} className="mt-3 mb-4">
+                        <p>Peso</p>
+                        <p className='text-black'>{weight} kg</p>
+                    </Grid>
+                    <Grid item xs={6} className="mb-5">
+                        <p>Preço</p>
+                        <p className='text-black'>P$ {price}</p>
+                    </Grid>
+                    <Grid item xs={6} className="mb-5">
+                        <p>Estoque</p>
+                        <p className='text-black'>{quantity}</p>
+                    </Grid>
+                </Grid>
+                <PokemonTierButton disabled className={`${getTierBGColor()} mr-4 mb-4 font-mono align-middle`}>{tier_name}</PokemonTierButton>
                 <div>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleAddToCart}>
                         Adicionar ao Carrinho
