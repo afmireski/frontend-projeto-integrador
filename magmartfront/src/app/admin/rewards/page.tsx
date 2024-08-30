a43296f6da8d9f6b16f2ddceae7aad38ed91b8bf
Alexandre <alexandretolomeotti@alunos.utfpr.edu.br>
2024-08-30 14:18:17 -0300

    feat: Add tab component to reward type selection

--- magmartfront/src/app/admin/rewards/page.tsx
+++ magmartfront/src/app/admin/rewards/page.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import RewardForm from '@/components/RewardForm';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import * as React from 'react';
 
 export default function Rewards() {
+  const initialTabSelected = 0;  
+  const [value, setValue] = React.useState(initialTabSelected);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <div>
       <Navbar />
@@ -15,10 +27,16 @@ export default function Rewards() {
             elevation={3} 
             sx={{ 
               padding: 4, 
-              backgroundColor: '#FFE0B2',  // Cor de fundo do formulário mais clara
+              backgroundColor: '#FFE0B2',
               borderRadius: '12px' 
             }}
           >
+            <div className="grid justify-items-center">
+              <Tabs value={value} onChange={handleChange} aria-label="disabled tabs example">
+                <Tab label="Pokémon" />
+                <Tab label="Futuras" disabled />
+              </Tabs>
+            </div>
             <Box sx={{ mt: 4 }}>
               <RewardForm />
             </Box>
