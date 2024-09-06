056b3ab01a7f7a9d9ea4270b4787a55ecd160172
gustavoreino <gustavoreino2003@gmail.com>
2024-09-05 21:56:09 -0300

    pagamento-formatado

---
+++ magmartfront/src/app/api/editCartItem.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { CartItem } from '@/components/myTypes/CartType';
+import Cookies from 'js-cookie';
+
+function EditCartItem(cart_id: string, item_id: string, quantity: number) {
+    const token = Cookies.get('authToken');
+    console.log('Token -> ', token);
+    return axios.patch<CartItem>(`${process.env.API_URL}/carts/${cart_id}/items/${item_id}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `${token}`, // Adiciona o token ao cabeçalho
+        },
+        body:
+        {
+            "quantity": quantity,
+        }
+    })
+        .then(response => {
+            return response.data;
+        })
+        .catch(error => {
+            throw ('error');
+        });
+}
+
+export default EditCartItem;
